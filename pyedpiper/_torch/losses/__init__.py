8ef4a2577ec19b94ab69d463594a82999880204b
Oleg Pavlovich <pavlovi4.o@gmail.com>
2020-07-14 14:00:07 +0700

    Rebuilt from scratch.
    Signed-off-by: Oleg Pavlovich <pavlovi4.o@gmail.com>

--- pyedpiper/_torch/losses/__init__.py
+++ pyedpiper/_torch/losses/__init__.py
@@ -1,8 +1,6 @@
-import importlib as _imp
-import os as _os
-
-
-# Automatically import any Python files in the losses directory
-for file in _os.listdir(_os.path.dirname(__file__)):
-    if file.endswith('.py') and not file.startswith('_'):
-        _imp.import_module(f"{__name__}." + file[:file.find('.py')])
+from .cauchy import *
+from .focal import *
+from .functional import *
+from .ohem_nll import *
+from .smooth import *
+from .wing import *
