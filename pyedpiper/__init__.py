8c317d18837571839f566b733ebf9ffe5cb08fba
Oleg Pavlovich <pavlovi4.o@gmail.com>
2020-07-14 18:41:05 +0700

    Okay, I hate PyCharm...

--- pyedpiper/__init__.py
+++ pyedpiper/__init__.py
@@ -1,1 +1,12 @@
+from ._random_name import get_random_name
+from ._common import (
+    as_numpy,
+    as_tensor,
+    call,
+    instantiate,
+    resolve_target,
+    set_random_seed,
+)
+
+
 __version__ = "0.1.5"
